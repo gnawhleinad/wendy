510a35114249834b456b65bee39490e827cbc14b
gnawhleinad <danielleehwang@gmail.com>
2014-08-12 01:39:04 -0700

    f09d2e1ab7b6a70688604e5aa182dca7db0dd297

--- wendy/jenkins/Jenkins.py
+++ wendy/jenkins/Jenkins.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import re
 import uuid
 
@@ -16,12 +17,57 @@ class Jenkins:
   def get_plugin_version(self, plugin):
     return re.match('.*\n{0}\s+[^\n]*?([\d\.]+)'.format(plugin),
                     self.cli.run('list-plugins'), 
-                    re.MULTILINE|re.DOTALL)
+                    re.MULTILINE|re.DOTALL).group(1)
 
   def add_credential(self, username, description, private_key_location):
-    return self._create_credentials(username, description, private_key_location)
+    credentials_file = os.path.join(self._parameters.home, 'credentials.xml')
 
-  def _create_credentials(self, username, description, private_key_location):
+    credentials = None
+    if os.path.isfile(credentials_file):
+      credentials = etree.parse(credentials_file)
+    else:
+      credentials = self._create_credentials()
+
+    credential_list = credentials.xpath(
+      '/{0}/{1}/{2}/{3}'.format(
+        'com.cloudbees.plugins.credentials.SystemCredentialsProvider',
+        'domainCredentialsMap',
+        'entry',
+        'java.util.concurrent.CopyOnWriteArrayList'))[0]
+
+    credential_list.append(self._add_credential(username, 
+                                                description, 
+                                                private_key_location))
+
+    credentials.write(credentials_file, 
+      xml_declaration=True, encoding='UTF-8', pretty_print=True)
+    # with open(credentials_file, 'w') as f:
+    #   f.write(etree.tostring(credentials, 
+    #             xml_declaration=True, encoding='UTF-8', pretty_print=True))
+
+  def _add_credential(self, 
+                      username, 
+                      description, 
+                      private_key_location):
+    name = \
+      'com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey'
+    credential = etree.Element(name,
+      plugin='ssh-credentials@{0}'.format(self.get_plugin_version(
+        'ssh-credentials')))
+
+    etree.SubElement(credential, 'scope').text = 'SYSTEM'
+    etree.SubElement(credential, 'id').text = str(uuid.uuid4())
+    etree.SubElement(credential, 'description').text = description
+    etree.SubElement(credential, 'username').text = username
+    etree.SubElement(credential, 'passphrase')
+    private_key = etree.SubElement(credential, 'privateKeySource',
+      attrib={'class':
+        '{0}$FileOnMasterPrivateKeySource'.format(name)})
+    etree.SubElement(private_key, 'privateKeyFile').text = private_key_location
+
+    return credential
+
+  def _create_credentials(self):
     credentials = etree.Element(
       'com.cloudbees.plugins.credentials.SystemCredentialsProvider',
       plugin='credentials@{0}'.format(self.get_plugin_version('credentials')))
@@ -34,21 +80,6 @@ class Jenkins:
       'com.cloudbees.plugins.credentials.domains.Domain')
     domain_credentials.append(etree.Element('specifications'))
 
-    ssh_credentials_element = \
-      'com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey'
-    cowal = etree.SubElement(entry, 'java.util.concurrent.CopyOnWriteArrayList')
-    ssh_credentials = etree.SubElement(cowal, ssh_credentials_element,
-      plugin='ssh-credentials@{0}'.format(self.get_plugin_version(
-        'ssh-credentials')))
-    etree.SubElement(ssh_credentials, 'scope').text = 'SYSTEM'
-    etree.SubElement(ssh_credentials, 'id').text = str(uuid.uuid4())
-    etree.SubElement(ssh_credentials, 'description').text = description
-    etree.SubElement(ssh_credentials, 'username').text = username
-    etree.SubElement(ssh_credentials, 'passphrase')
-    private_key = etree.SubElement(ssh_credentials, 'privateKeySource',
-      attrib={'class':
-        '{0}$FileOnMasterPrivateKeySource'.format(ssh_credentials_element)})
-    etree.SubElement(private_key, 'privateKeyFile').text = private_key_location
+    etree.SubElement(entry, 'java.util.concurrent.CopyOnWriteArrayList')
 
-    return etree.tostring(credentials,
-             xml_declaration=True, encoding='UTF-8', pretty_print=True)
+    return credentials.getroottree()
