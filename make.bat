d1a6a7000a008a942fe6ffef4e27cb4f9dc5ae85
Daniel <danielleehwang@gmail.com>
2014-08-20 13:54:12 -0700

    fix windows make test

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if "%1" == "test" (
-	vagrant up && vagrant ssh --command "temp=$(mktemp --directory) && rsync --recursive --exclude='.*' /vagrant/* $temp && chmod 644 test/*.py && sudo chgrp --recursive vagrant /var/lib/jenkins && sudo chmod --recursive g+w /var/lib/jenkins && cd $temp && nosetests --verbose"
+	vagrant up && vagrant ssh --command "temp=$(mktemp --directory) && rsync --recursive --exclude='.*' /vagrant/* $temp && chmod 644 $temp/test/*.py && sudo chgrp --recursive vagrant /var/lib/jenkins && sudo chmod --recursive g+w /var/lib/jenkins && cd $temp && nosetests --verbose"
 	if errorlevel 1 exit /b 1
 	goto end
 )
